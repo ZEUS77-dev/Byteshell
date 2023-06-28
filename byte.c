bbced49d270eabe1809502e36a30dbc15ebb2a8d
Devadutta Hati <71880311+ZEUS77-dev@users.noreply.github.com>
2023-06-28 17:40:03 +0530

    > chg

--- byte.c
+++ byte.c
@@ -67,7 +67,7 @@ void shell_loop() {
     int status;
 
     do {
-        printf("by8$hell> ");
+        printf(">$~ ");
         line = read_line();
         args = split_line(line);
         status = execute_command(args);
